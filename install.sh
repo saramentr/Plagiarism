9a892070ae11befeb2e8f24e83031ee7b6bc162b
Sara Mentr <saramentr@gmail.com>
2023-08-02 09:46:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@ pip3 install pydot==1.4.2
 pip3 install graphviz
 pip3 install scipy
 pip3 install pyparsing
-pip3 install process
+#pip3 install process
 pip3 install pathos
-git clone https://github.com/ShiftLeftSecurity/joern
-chmod -R a+rwx joern
+git clone https://github.com/joernio/joern
 cd joern
+git checkout tags/v1.1.450
+chmod -R a+rwx ./
 ./joern-install.sh
